704626a219a31b9cd17448626080fe3a81d0b71f
aanandmahawal <151204835+aanandmahawal@users.noreply.github.com>
2026-04-02 09:42:33 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,30 +1,18 @@
 import pandas as pd
 import joblib
 import os
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
-# Load data
-df = pd.read_csv("data/nasa_power_global_daily.csv")
+df = pd.read_csv("data/battery_data.csv")
 
 # Features & target
-features = ['T2M_MAX', 'T2M_MIN', 'RH2M', 'WS10M', 'PRECTOTCORR']
-target = 'T2M'
+X = df[['voltage', 'current', 'time']]
+y = df['temperature']
 
-X = df[features]
-y = df[target]
-
-# Train-test split
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-# Model
 model = RandomForestRegressor(n_estimators=50, max_depth=10)
-model.fit(X_train, y_train)
+model.fit(X, y)
 
-# Save model
 os.makedirs("model", exist_ok=True)
 joblib.dump(model, "model/model.pkl")
 
-print("✅ Model trained and saved!")
+print("✅ Model trained!")
